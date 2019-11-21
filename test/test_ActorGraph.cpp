0c182d1272cc598c3c1d744ad22a2ad0a0f290e1
Andrew R Masters <armaster@its-cseb230-37.ucsd.edu>
2019-11-21 15:37:52 -0800

    Adding tests

--- test/test_ActorGraph.cpp
+++ test/test_ActorGraph.cpp
@@ -6,3 +6,36 @@
 
 using namespace std;
 using namespace testing;
+
+class SimpleActorGraph: public ::testing::Test {
+  protected:
+
+  public:
+};
+
+TEST_F(SimpleActorGraph, Test_loadFromFile){
+	ActorGraph graph;
+
+	bool success = graph.loadFromFile("../testFile.txt", false);
+
+	graph.buildGraph();
+	
+	ASSERT_EQ(true, success);
+		
+}
+
+TEST_F(SimpleActorGraph, Test_loadFromFile_isEmpty){
+	ActorGraph graph;
+	
+	ASSERT_EQ(false, graph.loadFromFile("newTestFile.txt", false));
+		
+}
+
+TEST_F(SimpleActorGraph, Test_shortestPath){
+	ActorGraph graph;
+	
+	graph.loadFromFile("../testFile.txt", false);
+
+	
+			
+}
---
+++ test/test_pathFinder.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+#include "ActorGraph.hpp"
+
+using namespace std;
+using namespace testing;
+
+class SimplePathFinder: public ::testing::Test {
+  protected:
+
+  public:
+};
+
+TEST_F(SimplePathFinder, Test_loadTestPairs){
+
+}
