8b6a32ac68df1b45a800bbee4f8cb52bc2854dc2
Abdulkhaliq M Omar <amomar@its-cseb230-41.ucsd.edu>
2019-11-24 18:30:50 -0800

    link predictor started

--- src/linkpredictor.cpp
+++ src/linkpredictor.cpp
@@ -22,46 +22,123 @@
 
 using namespace std;
 
+struct WeightComparator {
+     bool operator()(pair<string,  int>& left, pair<string,  int>& right){
+	
+	 if(left.second == right.second){
+		return left.first > right.first;
+	
+	}
+        return left.second > right.second;
+    }
+};
 
-void predictLinks(Actor* actor){
-	priority_queue<Actor*, ActorComparator> collab;
-	priority_queue<Actor*, ActorComparator> notYetCollab; 
+vector<string> predictLinks(Actor* actor){
+      
+	       
+       //all neighbors of query actor's neighbors
+       vector<pair<Actor*, Movie*>>secondLevelNeighbors;
 	
 	//get all the adjacency list of this actor
 	vector<pair<Actor*, Movie*>> levelOne = actor->neighbors;
 	
 	
 	//used to count frequency of levelOne actors in levelOne actors' own adjacency list
-	vector<pair<string,int>> actorCount(actor->neighbors.size());
+	unordered_map<string,int> actorCount;
+        
+	//to reinstate original count of each neighbor
+	vector<int> originalCount(actor->neighbors.size());
 	
-	//initialize vector with names and initial count 
-	for( int i = 0; i < levelOne.size(); i++) {  =	
-	   actorCount[i].first =  (levelOne[i].first)->name;
-	   actorCount[i].second = 0;
-	
+	//initialize vector with names and frequency of each actor in query actor's list
+	for( int i = 0; i < levelOne.size(); i++) { 
+	   
+	    for( int j = 0; j < levelOne.size(); j++) {  
+	   
+	       if ( levelOne[i].first->name == levelOne[j].first->name){ 
+	       
+	            actorCount[(levelOne[i].first)->name]++;	
+	       } 
+	   
+	   }
+            originalCount[i] = actorCount[(levelOne[i].first)->name];    
 	}
 	
-	int counter = 0;
+
 	
 	//traverse through the adjacency list to find common actors with actor1
 	for( auto it = levelOne.begin(); it != levelOne.end(); ++it) {  
 		
 	    //get each of levelOne actor's list;
-	     vector<pair<Actor*, Movie*>>levelTwo = (*it)->neighbors;
+	     vector<pair<Actor*, Movie*>>levelTwo = (*it).first->neighbors;
 		
 	     for( auto iter = levelTwo.begin(); iter != levelTwo.end(); ++iter) {  
-	         
-		  //if we have found actor in neighbors list
-	         if((*it)->name.compare((*iter)->name) == 0) { 
-		 
-		  actorCount[i].second += 1;//increment count for this actor;	    
-		 }
-		 i++;	     
+                  
+		   //get all neighbors of query actor's neighbors;
+		  if(find(levelOne.begin(), levelOne.end(), *iter) == levelOne.end())  { 	 
+		 	 //add to list of second level neighbors for query actor
+		  	secondLevelNeighbors.emplace_back((*iter));
+		  }
+		      
 	     }
 	}
-}
+        
+		
+	//map of each second level neighbor and its weight
+	priority_queue< pair<string, int>, vector<pair<string, int>>, WeightComparator>  levelTwoWeight;
 	
-bool loadTestPairs(string in_filename, vector<pair<string, string>>& pair) {
+	for( int i = 0; i < secondLevelNeighbors.size(); i++) { 
+	  
+	   //the weight of each levelTwo neighbor
+	   int weight = 0;
+	    
+	   for(auto iter = secondLevelNeighbors[i].first->neighbors.begin(); iter != secondLevelNeighbors[i].first->neighbors.end(); ++iter){
+	    
+                 //if we have a level one actor in levelTwo's neighbors
+	         if(find(levelOne.begin(), levelOne.end(), *iter) == levelOne.end()) {  
+	    
+		     auto it = actorCount.find((*iter).first->name);
+		 
+		     //update the count  
+		     if(it != actorCount.end()) 
+		    
+		        it->second = (it->second * 2);//double the weight	    	
+	         }
+	   
+           }
+           
+	   //computer total weight for each level two neighbor
+	   for( auto it = actorCount.begin(); it != actorCount.end(); ++it) { 
+	     		 
+	       weight += (*it).second;      
+	   }
+           
+	   //get average weight for each levelTwo neighbor;
+	   int averageWeight = weight/actorCount.size();
+           
+	   //insert levelTwo neighbor with its weight on to list
+	   levelTwoWeight.emplace(secondLevelNeighbors[i].first->name, averageWeight);
+	   
+	   //reset count map to original count	
+	   for( int i = 0; i < actorCount.size(); i++) {  	
+	      
+	       actorCount[(levelOne[i].first)->name] = originalCount[i];	
+	   }
+       }
+
+	vector<string> topFour;
+	//return top 4 weights in priority queue
+	for(int i = 0; i < 4; i++){
+	    topFour.emplace_back(levelTwoWeight.top().first);	
+	    levelTwoWeight.pop();
+	}
+
+	return topFour;
+
+ }
+
+
+bool loadQueryActor(string in_filename, vector<string> queryActors) {
+  /*  
     // Initialize the file stream
     ifstream infile(in_filename);
 
@@ -89,19 +166,18 @@ bool loadTestPairs(string in_filename, vector<pair<string, string>>& pair) {
 
             // get the current string before hitting a tab character and put
             // it in 'str'
-            if (!getline(ss, str, '\t')) break;
+            if (!getline(ss, str, '\n')) break;
             record.push_back(str);
         }
 
-        if (record.size() != 2) {
+        if (record.size() != 1) {
             // we should have exactly 2 columns
             continue;
         }
 
-        string actor1(record[0]);
-        string actor2(record[1]);
-
-        pair.emplace_back(make_pair(actor1, actor2));  // add to list of pairs
+        string actor(record[0]);
+       
+        queryActors.emplace_back(actor);  // add to list of pairs
     }
     if (!infile.eof()) {
         cerr << "Failed to read " << in_filename << "!\n";
@@ -110,20 +186,21 @@ bool loadTestPairs(string in_filename, vector<pair<string, string>>& pair) {
 
     infile.close();
     return true;
+    */
 }
 
 int main(int argc, char* argv[]) {
     string database(argv[1]);  // database file
 
-    string pairsFile(argv[2]);  // the file containing the pairs
+    //string pairsFile(argv[2]);  // the file containing the pairs
 
-    ofstream out;
+    //ofstream out;
 
-    out.open(argv[3]);
+    //out.open(argv[3]);
 
-    vector<pair<string, string>> pairs;
+    //vector<pair<string, string>> pairs;
 
-    bool success = loadTestPairs(pairsFile, pairs);  // load the test pairs
+    //bool success = loadTestPairs(pairsFile, pairs);  // load the test pairs
 
     ActorGraph graph;
 
@@ -132,7 +209,17 @@ int main(int argc, char* argv[]) {
     graph.buildGraph();  // build the graph to search
 
     // write the header of the file first
-    out << "(actor)--[movie#@year]-->(actor)--..." << endl;
+    //out << "(actor)--[movie#@year]-->(actor)--..." << endl;
+
+    Actor* actor = new Actor( "Kevin Bacon");
+    auto it = graph.actors.find(actor);
+    vector<string> topFour = predictLinks(*it);
+
+    for( int i = 0; i < topFour.size(); i++) {  
+    
+    
+       cout<<topFour[i]<<endl;
+    }
 
 
 
