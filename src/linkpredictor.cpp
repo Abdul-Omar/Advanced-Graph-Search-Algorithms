9a71cdf954f810fb21b59cdf93293063530cd6d1
Andrew R Masters <armaster@its-cseb240-37.ucsd.edu>
2019-12-05 16:50:51 -0800

    Added all headers, done now hopefully

--- src/linkpredictor.cpp
+++ src/linkpredictor.cpp
@@ -1,7 +1,8 @@
 /*
  * linkpredictor.cpp
- * Author: <YOUR NAME HERE>
+ * Author: Andrew Masters, Abdulkhaliq Omar
  *
+ * this file implements the functions that run the linkpredictor program
  */
 
 #include <bits/stdc++.h>
@@ -21,6 +22,9 @@
 
 using namespace std;
 
+// Compares the weights that are used on edges; used by predictLinksCollab
+// and predictLinkUncollab.
+//
 struct WeightComparator {
     bool operator()(pair<string, int>& left, pair<string, int>& right) {
         if (left.second == right.second) {
@@ -29,10 +33,11 @@ struct WeightComparator {
         return left.second < right.second;
     }
 };
-/* shortestPath : this function takes in two arguments both being of the class
- * Actor in order to find the shortest path between the two. This function uses
- * the concept of a BFS to determine the shortest number of movies between two
- * different actors
+
+/* Finds all the second neighbors of actor1 using a breadth first search
+ * Is used in predictLinkUncollab
+ * Takes an actor to search for second neighbors, the set of all actors, and
+ * level to determine if were at the second level
  */
 vector<Actor*> BFS(Actor*& actor1, set<Actor*, ActorComparator>& actors,
                    int level) {
@@ -90,7 +95,16 @@ vector<Actor*> BFS(Actor*& actor1, set<Actor*, ActorComparator>& actors,
     return levelTwo;
 }
 
-void predictLinks2(Actor* actor, set<Actor*, ActorComparator>& actors,
+/* Predicts how likely the actor that is given will collaborate with actors
+ * previously collaborated with. First, find all actors on the first level and
+ * get the frequency of each one. Then calculate the second level neighbors.
+ * We then determine the weights of both the first and second levels to determine
+ * priority. After entering all the first neighbors into a queue, we then insert
+ * the top four collaborated actors into the given file.
+ * Parameters include an actor, the set containing all actors, and the file we
+ * enter the top four collaborated into.
+ */
+void predictLinksCollab(Actor* actor, set<Actor*, ActorComparator>& actors,
                    ofstream& out) {
     // get all the adjacency list of this actor
     vector<pair<Actor*, Movie*>> levelOne = actor->neighbors;
@@ -176,7 +190,17 @@ void predictLinks2(Actor* actor, set<Actor*, ActorComparator>& actors,
     out << '\n';
 }
 
-void predictLinks(Actor* actor, set<Actor*, ActorComparator>& actors,
+/* Predicts how likely the actor that is given will collaborate with actors
+ * not yet collaborated with. First, find all actors on the first level and
+ * get the frequency of each one. Then find the second level neighbors by calling
+ * the BFS function.
+ * We then determine the weights of both the first and second levels to determine
+ * priority. After entering all the first neighbors into a queue, we then insert
+ * the top four collaborated actors into the given file.
+ * Parameters include an actor, the set containing all actors, and the file we
+ * enter the top four not yet collaborated into.
+ */
+void predictLinksUncollab(Actor* actor, set<Actor*, ActorComparator>& actors,
                   ofstream& out) {
     // all neighbors of query actor's neighbors
     vector<Actor*> secondLevelNeighbors;
@@ -264,6 +288,11 @@ void predictLinks(Actor* actor, set<Actor*, ActorComparator>& actors,
     out << endl;
 }
 
+/* Load all actors that are in the given file into the vector to be returned
+ * All actors will be added into a vector that we will determine the collaborated
+ * and the not yet collaborated.
+ * Parameters include the file to input and the vector of actors to be filled
+ */
 bool loadQueryActors(string in_filename, vector<string>& queryActors) {
     // Initialize the file stream
     ifstream infile(in_filename);
@@ -312,10 +341,16 @@ bool loadQueryActors(string in_filename, vector<string>& queryActors) {
     return true;
 }
 
+/* Main function:
+ * loads all actors from the first file argument and creates the graph
+ * loads all actors to be search for collaborated and uncollaborated
+ * functions predictLinksCollab and predictLinkUncollab are run for all
+ * actors and the output is put in their respective files.
+ */
 int main(int argc, char* argv[]) {
-    ofstream out(argv[3]);  // open collab file for writing
+    ofstream collabFile(argv[3]);  // open collab file for writing
 
-    ofstream out2(argv[4]);  // open uncollab file for writing
+    ofstream uncollabFile(argv[4]);  // open uncollab file for writing
 
     vector<string> actors;
 
@@ -334,8 +369,8 @@ int main(int argc, char* argv[]) {
     graph.buildGraph();  // build the graph to search
 
     // write the header of the file first
-    out << "Actor1,Actor2,Actor3,Actor4" << endl;
-    out2 << "Actor1,Actor2,Actor3,Actor4" << endl;
+    collabFile << "Actor1,Actor2,Actor3,Actor4" << endl;
+    uncollabFile << "Actor1,Actor2,Actor3,Actor4" << endl;
 
     for (int i = 0; i < actors.size(); i++) {
         Actor* actor = new Actor(actors[i]);
@@ -344,9 +379,9 @@ int main(int argc, char* argv[]) {
 
         if (it == graph.actors.end()) return 0;
 
-        predictLinks(*it, graph.actors, out2);
+        predictLinksUncollab(*it, graph.actors, uncollabFile);
 
-        predictLinks2(*it, graph.actors, out);
+        predictLinksCollab(*it, graph.actors, collabFile);
 
         delete (actor);
     }
